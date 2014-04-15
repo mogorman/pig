01f78a026720acc71ec1cd7b373f509d7640f87b
Matthew O'Gorman <mog@rldn.net>
2014-04-15 15:52:30 -0500

    okay this time get it all in

--- firmware/application/application.ino
+++ firmware/application/application.ino
@@ -13,15 +13,15 @@ int led = 4;
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  Serial.begin(57600);
+  Serial.begin(9600);
   pinMode(led, OUTPUT);     
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);               // wait for a second
+  delay(200);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(100);               // wait for a second
-  Serial.println("hello there!");
+  Serial.println("hello there!!");
 }
