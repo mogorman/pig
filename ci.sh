ec41aa2b1cade08f884666ecba5eb3e8dc5a2a27
Matthew O'Gorman <mog@rldn.net>
2014-06-06 12:26:27 -0500

    yup

--- ci.sh
+++ ci.sh
@@ -10,8 +10,8 @@ make CUSTOMPCB="/usr/src/pcb/src/pcb" \
      CUSTOMPCB_ARGS="--photo-mask-colour red \
      --photo-silk-colour white  --photo-plating  tinned" photos
 cd ..
-cp hardware/schematic.png ../pig/artifacts/schematic.png
-cp hardware/schematic.png ../pig/artifacts/`git rev-parse --short HEAD`_schematic.png
+cp hardware/schematic.png ~/pig/artifacts/schematic.png
+cp hardware/schematic.png ~/pig/artifacts/`git rev-parse --short HEAD`_schematic.png
 
-cp hardware/board.png ../pig/artifacts/board.png
-cp hardware/board.png ../pig/artifacts/`git rev-parse --short HEAD`.png
+cp hardware/board.png ~/pig/artifacts/board.png
+cp hardware/board.png ~/pig/artifacts/`git rev-parse --short HEAD`.png
