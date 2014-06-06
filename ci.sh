feae078f4d0de8e777b75951dd32aa8d0c292a58
Matthew O'Gorman <mog@rldn.net>
2014-06-06 14:39:00 -0500

    this change should stop bloat because not everything changes all the time.

--- ci.sh
+++ ci.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 #this is where continuous integration script will live
+
+copy_files() {
+    local new_copy="$1"
+    local old_copy="$2"
+    local versioned=`git rev-parse --short HEAD`"_$1"
+    if diff "$new_copy" "$old_copy" 2>/dev/null ; then
+	echo Same file nothing to do
+    else
+	echo File changed make copies.
+	cp "$1" "$2"
+	cp "$1" "~/artifacts/pig/"`git rev-parse --short HEAD`_"$1"
+    fi    
+}
+
 git submodule update --init
 ls -la
 cd hardware
+
+if diff file1 file2 2>/dev/null ; then
+  echo Same
+else
+  echo Different
+fi
 
 make CUSTOMPCB="/usr/src/pcb/src/pcb" \
      CUSTOMPCB_ARGS="--photo-mask-colour red \
@@ -10,21 +30,34 @@ make CUSTOMPCB="/usr/src/pcb/src/pcb" \
 make CUSTOMPCB="/usr/src/pcb/src/pcb" \
      CUSTOMPCB_ARGS="--photo-mask-colour red \
      --photo-silk-colour white  --photo-plating  tinned" photos
-cp schematic.png ~/artifacts/pig/schematic.png
-cp schematic.png ~/artifacts/pig/`git rev-parse --short HEAD`_schematic.png
 
-cp board.png ~/artifacts/pig/board.png
-cp board.png ~/artifacts/pig/`git rev-parse --short HEAD`.png
 
-cp *.zip ~/artifacts/pig/gerbers.zip
+if diff file2 2>/dev/null ; then
+  echo Same
+else
+  echo Different
+fi
+
+copy_files schematic.png ~/artifacts/pig/schematic.png
+
+copy_files board.png ~/artifacts/pig/board.png
+
+cp *.zip gerbers.zip
+
+copy_files gerbers.zip ~/artifacts/pig/gerbers.zip
 
 cd ..
 
 cd firmware
 
 make ARDUINO=/usr/src/arduino-1.5.6-r2/arduino
-cp bin/bootloader.hex ~/artifacts/pig/
-cp bin/match.hex ~/artifacts/pig/
-cp bin/application.hex ~/artifacts/pig/
+
+cd bin
+
+copy_files bootloader.hex ~/artifacts/pig/bootloader.hex
+copy_files match.hex ~/artifacts/pig/match.hex
+copy_files application.hex ~/artifacts/pig/application.hex
+
+cd ..
 
 cd ..
